69342158664f0b4c34fcb873c1a4c41f0d932628
Andrii Shkabrii <shkabriy.a@gmail.com>
2021-07-20 17:05:14 +0300

    Initial Commit

---
+++ BeTeacher/BeTeacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeTeacherApp.swift
+//  BeTeacher
+//
+//  Created by MacBook on 20.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct BeTeacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
